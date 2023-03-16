0fe481030e8946f06d57e56ebba12174611663b1
emanali <emanali.12.1996@gmail.com>
2023-03-16 12:10:42 +0300

    Appointment & Schedule models

--- HealthCare/main_app/models.py
+++ HealthCare/main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Appointment(models.Model):
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    cancellation = models.BooleanField()
+    Date = models.DateField()
+
+    # doctor = models.ForeignKey('Doctor')
+    # patient = models.ForeignKey('Patient')
+
+class Schedule (models.Model):
+    start = models.TimeField()
+    end= models.TimeField()
